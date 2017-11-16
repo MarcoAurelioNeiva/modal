4f8f6774c8a3f168a0ff2da406159a8d5ebc72a7
MarcoAurelioNeiva <marco_neiva@hotmail.com>
2017-11-16 15:56:32 -0200

    Colocando historia de Jude / Lucy

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 	}
 	.modal-header{
 		padding: 2px 16px ;
-		background-color: #5cb85c;
+		background-color: black;
 		color: white;
 		display:flex;
 		justify-content:space-between;
@@ -44,12 +44,15 @@
 	}
 	.modal-header h2{
 		flex-grow:1;
-		margin: 0 12px;
+		margin: 0 150px;
 	}
-	.modal-body{padding:2px 16px;}
+	.modal-body{
+		padding:2px 16px;
+		background-color: rgb(192,192,192);
+	}
 	.modal-footer{
-		padding: 2px 16px;
-		background-color:#5cb85c;
+		padding: 10px 16px;
+		background-color:black;
 		color: white;
 	}
 
@@ -63,15 +66,16 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<span class="close">&times;</span>				
-				<h2>modal Header</h2>		
+				<h2>Jude / Lucy</h2>		
 			</div>
 
 			<div class="modal-body">
-				<p>123</p>
-				<p>321</p>
+				<p>Morador(a) da Vila do Sol, não conhece seu passado. O que ele(a) sabe é que em um dia de tempestade, foi abandonado(a) na porta da capela da vila por uma figura misteriosa e foi criado por seus pais adotivos. Treinou a vida inteira para realizar o sonho de entrar no exército e lutar para proteger o reino. Após o saque à Vila do Sol, irá se aventurar ao lado de Walrus, seu(ua) mentor, para encontrar o responsável pelo ataque e descobrir sobre sua descendência e seu estranho poder. Ao longo da história, descobrirá que faz parte de uma antiga profecia que decidirá o destino do mundo.
+</p>
+				
 			</div>
 			<div class="modal-footer">
-				<h3>iiiiaiii</h3>
+				<h3></h3>
 			</div>
 		</div>
 
